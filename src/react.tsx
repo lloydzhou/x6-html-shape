22f5d1a147fd49eb32ed0fd688c2972784880498
lloydzhou <lloydzhou@qq.com>
2023-04-12 22:53:38 +0800

    add react render: react18+react17+portal

---
+++ src/react.tsx
@@ -0,0 +1,12 @@
+import { createRoot } from 'react-dom/client'
+
+export default function createRender(Component) {
+  return function render(node, graph, container) {
+    const root = createRoot(container)
+    root.render(<Component node={node} graph={graph} />)
+    return () => root.unmount()
+  }
+}
+
+
+
---
+++ src/react17.tsx
@@ -0,0 +1,11 @@
+import * as ReactDOM from 'react-dom';
+
+export default function createRender(Component) {
+  return function render(node, graph, container) {
+    ReactDOM.render(<Component node={node} graph={graph} />, container)
+    return () => ReactDOM.unmountComponentAtNode(container)
+  }
+}
+
+
+
